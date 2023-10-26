3182d69f3626c5b8acbed136c3db792609976c8c
NA\GIORGIO.CAIZZI <giocaizzi@gmail.com>
2023-10-26 13:59:54 +0200

    export_maps

--- pyarcgis/core.py
+++ pyarcgis/core.py
@@ -5,39 +5,15 @@ import arcpy
 
 MAPS_EXCLUDE_PATTERN = ["template","00"]
 
+LAYOUT_TEXT_ELEMENTS = [
+    r"{{PROJ}}"
+    ]
 
-class Project:
-    _path = None
-
-    def __init__(self, path):
-        self.path = path
-
-        
-    @property
-    def path(self):
-        return self._path
-    
-    @path.setter
-    def path(self, path):
-        self._path = os.path(path)
-
-    @property
-    def maps(self):
-        return _get_files_by_extension(self.path, ".mxd",exclude_patterns=MAPS_EXCLUDE_PATTERN)
-
-
-    def export_all_maps(self, output_folder=None,verbose=False):
-        for mxd in self.maps:
-            if verbose:
-                print("Exporting map: {}".format(mxd))
-            with Map(mxd) as imap:
-                imap.export_to_pdf()
-            if verbose:
-                print("Map exported: {}".format(mxd))
-
-import fnmatch
-
-def _get_files_by_extension(root, extension,exclude_patterns=None):
+def _get_files_by_extension(root, extension, exclude_patterns=None, subset_pattern=None):
+    if exclude_patterns and not isinstance(exclude_patterns, list):
+        exclude_patterns = [exclude_patterns]
+    if subset_pattern and not isinstance(subset_pattern, list):
+        subset_pattern = [subset_pattern]
     files_list = []
     for root, subdirs, files in os.walk(root):
         for file in files:
@@ -45,29 +21,63 @@ def _get_files_by_extension(root, extension,exclude_patterns=None):
                 if exclude_patterns:
                     if any(pattern in file for pattern in exclude_patterns):
                         continue
-                files_list.append(os.path.join(root, file))
+                if subset_pattern is None or any(pattern in file for pattern in subset_pattern):
+                    files_list.append(os.path.join(root, file))
     return files_list
+
+
+class Project:
+    _path = None
+
+    def __init__(self, path):
+        self._path = path
+     
+    @property
+    def path(self):
+        return self._path
+
+    
+    def get_maps(self,subset_pattern=None):
+        return _get_files_by_extension(self.path, ".mxd",exclude_patterns=MAPS_EXCLUDE_PATTERN,subset_pattern=subset_pattern)
+
+
+    def export_maps(self,maps,verbose=False):
+        for mxd in maps:
+            if verbose:
+                print("Exporting map: {}".format(mxd))
+            with Map(mxd) as imap:
+                imap.export_to_pdf()
+            if verbose:
+                print("Map exported: {}".format(mxd))
 
 
 class Map:
     _mxd_path = None
+    _mxd_basename = None
+    _mxd_filename = None
+    _mxd_folder = None
     _map = None
 
-    # must be used with a context manager
+    # must be used with a context manager to ensure the MapDocument object is deleted
+    # after use (close the file)
 
-    def __init__(self, mxd_path):
+    def __init__(self, mxd_path,save_on_exit=False):
         if not mxd_path.endswith(".mxd"):
             raise AttributeError("The path must be a .mxd file")
         self._mxd_path = mxd_path
-        self._mxd_filename = os.path.basename(mxd_path)
+        self._mxd_basename = os.path.basename(mxd_path)
+        self._mxd_filename, _ = os.path.splitext(self._mxd_basename)
         self._mxd_folder = os.path.dirname(mxd_path)
         self._map = arcpy.mapping.MapDocument(self._mxd_path)
+        self._save_on_exit = save_on_exit
 
     def __enter__(self):
         return self
     
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, traceback):
         # Clean up the MapDocument object by deleting it.
+        if self._save_on_exit:
+            self.save()
         del self._map
 
     def export_to_pdf(self):
@@ -78,3 +88,21 @@ class Map:
         except Exception as e:
             print("Error exporting map: {}".format(self._mxd_path))
             raise e
+        
+    def set_template_layout_text_elements(self,layout_elements_map):
+        for element in LAYOUT_TEXT_ELEMENTS:
+            self.set_text_element(element, layout_elements_map[element])
+        
+    def set_text_element(self, old_text, new_text):
+        for element in self._get_layout_elements():
+            if element.text == old_text:
+                element.text = new_text
+
+    
+        
+
+    def _get_layout_elements(self, element_type="TEXT_ELEMENT"):
+        return arcpy.mapping.ListLayoutElements(self._map, element_type)
+    
+    def save(self):
+        self._map.save()
